ce46359dbb1d689f09ece809a6f28fd5ac4e33cc
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-04-25 19:28:17 -0300

    Submit challenge again

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,25 +9,71 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+  
+const valid_repository = (request, response, next) => {
+  const { id } = request.params;
+  const idx = repositories.findIndex(r => id == r.id);
+  
+  if(idx == -1) 
+    return response.status(400).json('Repository not find');
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+  request.repository = repositories[idx];
+  request.repositoryIndex = idx;
 
+  return next();
+}
+
+// LIST REPOSITORIES 
+app.get("/repositories", (request, response) => response.json(repositories));
+
+// CREATE REPOSITORY 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes : 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+// EDIT REPOSITORY
+app.put("/repositories/:id", valid_repository, (request, response) => {
+  const repository = request.repository;
+  const { title, url, techs } = request.body;
+  const obj = { title, url, techs };
+  
+  for(atr in obj) {
+    if(!obj[atr]) continue;
+    repository[atr] = obj[atr];
+  }
+  
+  return response.json(repository);
+}); 
+
+// DELETE REPOSITORY
+app.delete("/repositories/:id", valid_repository, (request, response) => {
+  const { id } = request.params;
+  const idx = request.repositoryIndex;
+
+  repositories.splice(idx, 1);
+
+  return response.status(204).json();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+// LIKE REPOSITORY
+app.post("/repositories/:id/like", valid_repository, (request, response) => {
+  const repository = request.repository;
+
+  repository.likes++;
+
+  return response.json(repository);
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
 
 module.exports = app;
